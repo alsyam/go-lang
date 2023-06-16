de191f6342b5c9221ccb03f7f12c929b85a60ce6
alsyam <malsyam69@gmail.com>
2023-06-16 23:42:16 +0700

    add file string and variable

---
+++ golang-dasar/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad")
+	fmt.Println("Muhammad Alsyam")
+
+	fmt.Println(len("Muhammad"))
+	fmt.Println("Muhammad Alsyam"[4])
+}
---
+++ golang-dasar/variable.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Muhammad Alsyam"
+	fmt.Println(name)
+
+	name = "Muhammad Al"
+	fmt.Println(name)
+
+	var friendName = "Muhammad Al"
+	fmt.Println(friendName)
+
+	var age = 22
+	fmt.Println(age)
+
+	country := "indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "muhammad"
+		lastName  = "alsyam"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+}
